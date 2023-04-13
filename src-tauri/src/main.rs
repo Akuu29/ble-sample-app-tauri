a4dc81f38d8a530c84cc84f3b72dd84f74254c5e
AKUU <57586401+Akuu29@users.noreply.github.com>
2023-04-13 23:08:01 +0900

    Add Add function to retrieve advertised devices

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,66 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter};
+use btleplug::platform::{Adapter, Manager};
+use std::time::Duration;
+use tokio::time;
+
+// mod bluetooth;
+
+/// アドバタイズしているデバイスの一覧を取得する
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn get_devices() -> Result<Vec<String>, String> {
+    let manager = Manager::new().await.unwrap();
+
+    // アダプタの作成
+    let result_get_adapters = manager.adapters().await;
+    let adapters = match result_get_adapters {
+        Ok(adapters) => adapters,
+        Err(_) => return Err("Can not get adapters".to_string()),
+    };
+    let central = adapters.into_iter().nth(0).unwrap();
+
+    // アドバタイズしているデバイスのスキャン
+    let start_scan_result = central.start_scan(ScanFilter::default()).await;
+    match start_scan_result {
+        Ok(_) => (),
+        Err(_) => return Err("Failed to start scan".to_string()),
+    }
+    // 5秒間wait
+    time::sleep(Duration::from_secs(5)).await;
+
+    // 取得したデバイスの名称
+    let device_name = get_device_name(central).await;
+
+    dbg!(&device_name);
+    Ok(device_name)
+}
+
+/// デバイスの名称を取得
+async fn get_device_name(central: Adapter) -> Vec<String> {
+    let mut device_name = vec![];
+    for p in central.peripherals().await.unwrap() {
+        let properties = p.properties().await.unwrap();
+        if let Some(data) = properties {
+            if let Some(name) = data.local_name {
+                device_name.push(name);
+            } else {
+                continue;
+            }
+        } else {
+            continue;
+        }
+    }
+
+    device_name
 }
 
 fn main() {
+    // use tauri::async_runtime::block_on;
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_devices])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
