43ba95b578980a3efc1f1357bd80386806bde3d0
AKUU <57586401+Akuu29@users.noreply.github.com>
2023-04-22 21:27:49 +0900

    Update Update React GUI. Add button "GET Device". A list of devices acquired by the "GET Device" handler is displayed, and  "Connect" button is provided for each.

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,46 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+type Devices = Array<string>;
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+const App = () => {
+  const [devices, setDevices] = useState<Devices | null>(null);
+
+  const handleGetDevices = async () => {
+    const deviceList = await invoke<Devices>("get_devices")
+      .catch(err => {
+        console.error(err);
+        return null;
+      });
+
+    setDevices(deviceList);
+  };
+
+  const handleConnect = async (deviceName: string) => {
+    // 接続
+    await invoke<void>("connect_device", { deviceName })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <button onClick={handleGetDevices}>Get Device</button>
+      <div>
+        {devices != null && (
+          <ul>
+            {devices.map((device, index) => (
+              <li key={index}>
+                {device}
+                <button onClick={() => handleConnect(device)}>Connect</button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
-      <p>{greetMsg}</p>
-    </div>
-  );
-}
+    </>
+  )
+};
 
 export default App;
